1698ed20ef2236012f7f049e9c976110f2c93590
LUIS SILVA <luis.roberto.silva.guillen@gmail.com>
2019-04-07 22:39:46 -0400

    Avance

---
+++ src/app/pages/pages.routing.module.ts
@@ -0,0 +1,26 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { SecurepagesComponent } from './securepages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphs1Component } from './graphs1/graphs1.component';
+
+const routes: Routes = [
+    {
+      path:'', 
+      component: SecurepagesComponent, 
+      children:[
+        {path:'dashboard', component: DashboardComponent},
+        {path:'progress', component: ProgressComponent},
+        {path:'graficas-1', component: Graphs1Component},
+        {path:'', redirectTo:'/dashboard', pathMatch: 'full'},
+      ]
+    }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class PegesRoutingModule { }
